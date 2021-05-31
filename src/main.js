0e715c373bb88450550697e9c94bbeca5470eafe
zzhaicn <zhengmurong168@sina.com>
2021-05-31 13:54:59 +0800

    初始化组件引入

--- src/main.js
+++ src/main.js
@@ -2,7 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Col, Row , Field, CellGroup,Icon,Swipe, SwipeItem,Picker,Popup,Button,Tab,Tabs } from 'vant';
 
+//Vue.use(Swiper)
+
+Vue.use(Col)
+Vue.use(Row)
+Vue.use(Field)
+Vue.use(CellGroup)
+Vue.use(Icon)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Picker)
+Vue.use(Popup)
+Vue.use(Button)
+Vue.use(Tab);
+Vue.use(Tabs);
 Vue.config.productionTip = false
 
 new Vue({
