18f83657caf0d056c07004033e29ed7859b2e134
zzhaicn <zhengmurong168@sina.com>
2021-05-31 13:50:22 +0800

    添加路由

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,72 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/sidemeasure',
+    name: 'SideMeasure',
+    component: () => import(/* webpackChunkName: "SideMeasure" */ '../views/SideMeasure.vue')
+  },
+  {
+    path: '/chestmeasure',
+    name: 'ChestMeasure',
+    component: () => import(/* webpackChunkName: "ChestMeasure" */ '../views/ChestMeasure.vue')
+  },
+  {
+    path: '/upchestmeasure',
+    name: 'UpChestMeasure',
+    component: () => import(/* webpackChunkName: "UpChestMeasure" */ '../views/UpChestMeasure.vue')
+  },
+  {
+    path:'/chestshapes',
+    name: 'ChestShapes',
+    component: () => import(/* webpackChunkName: "ChestShapes" */ '../views/ChestShapes.vue')
+  },
+  {
+    path:'/brandchoice',
+    name: 'brandChoice',
+    component: () => import(/* webpackChunkName: "ChestShapes" */ '../views/BrandChoice.vue')
+  },
+  {
+    path:'/result',
+    name: 'result',
+    component: () => import(/* webpackChunkName: "Result" */ '../views/Result.vue')
+  },
+  {
+    path:'/nosize',
+    name: 'nosize',
+    component: () => import(/* webpackChunkName: "NoSize" */ '../views/NoSize.vue')
+  },
+  {
+    path:'/hadsize',
+    name: 'hadsize',
+    component: () => import(/* webpackChunkName: "HadSize" */ '../views/HadSize.vue')
+  },
+  {
+    path:'/fitbar',
+    name: 'fitbar',
+    component: () => import(/* webpackChunkName: "FitBar" */ '../views/FitBar.vue')
+  },
+  {
+    path:'/cupfit',
+    name: 'cupfit',
+    component: () => import(/* webpackChunkName: "CupFit" */ '../views/CupFit.vue')
+  },
+  {
+    path:"/furu",
+    name:'furu',
+    component: () => import(/* webpackChunkName: "FuRu" */ '../views/FuRu.vue')
+  },
+  {
+    path:"/jixiong",
+    name:'jixiong',
+    component: () => import(/* webpackChunkName: "JiXiong" */ '../views/JiXiong.vue')
+  },
+  {
+    path:"/backfit",
+    name:'backfit',
+    component: () => import(/* webpackChunkName: "JiXiong" */ '../views/BackFit.vue')
   }
+
+
 ]
 
 const router = new VueRouter({
