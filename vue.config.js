c064dad9fe2782524d9c139a94bdd8bc5f8b7bdb
zzhaicn <zhengmurong168@sina.com>
2021-08-29 13:58:18 +0800

    新的打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const CompressionPlugin = require('compression-webpack-plugin');
 module.exports = {
+  chainWebpack: config => {
+    config.module.rule('images').use('url-loader').loader('url-loader').tap(options=>{
+      options.fallback.options.name='img/[name].[ext]'
+      return options
+    })
+  },
+  productionSourceMap: false,
    configureWebpack: {
         plugins: [
             new CompressionPlugin({
