283c59133228321ef304b8710f3a820bdbdefd4e
zzhaicn <zhengmurong168@sina.com>
2021-08-29 13:48:33 +0800

    添加新组件

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
   </div>
 </template>
 <script>
-  //import Vue from 'vue'
   export default{
 
     data(){
@@ -26,22 +25,20 @@
     },
     created(){
       document.querySelector("meta[name='viewport']")["content"] = "width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no"
-
     },
     mounted(){
       document.getElementsByTagName("body")[0].className="bodybg"
+
     },
     updated(){
-      //this.nickname3=this.$store.state.nickname
 
     }
   }
 </script>
 <style lang="scss">
-.bodybg{background:#fff0f0;overflow-y:auto;}
+
 @media only screen and (min-width:480px){
   #app{
-    //background:#fff5f5;
     width:480px;
     margin:0 auto;
     font-size:0.8rem;
@@ -51,14 +48,12 @@
     text-align: center;
     color: #2c3e50;
     position: relative;
-  //  box-sizing: content-box;
 
   }
 }
 @media only screen and (max-width:480px) {
 
   #app {
-    //background:#fff5f5;
     width:100%;
     font-size:0.8rem;
     font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -69,18 +64,6 @@
     color: #2c3e50;
 
   }
-
-  #nav {
-    padding: 30px;
-
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-    }
-  }
-
 }
+.bodybg{background:#fff0f0;overflow-y:auto;}
 </style>
